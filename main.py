c51a280336512a87289dea0bd04a37b6f4f73b02
anadan10102006 <141078706+anadan10102006@users.noreply.github.com>
2023-08-02 23:50:39 +0000

    Ejercicio 9

--- main.py
+++ main.py
@@ -1,3 +1,59 @@
-#buenaaas 
-Hello=('Hola Mundo')
-print(Hello)
+#buenaaas
+#Ejercicio 1
+'''print('Hola mundo')'''
+
+#Ejercicio 2
+'''Hello=('Hola Mundo')
+print(Hello)'''
+
+#Ejercicio 3
+'''print('¿Como te llamas?')
+nombre=input() 
+print('Es un placer conocerte, ' + nombre + '.')'''
+
+#Ejercicio 4
+'''print(((3+2)/(2*5))**2)'''
+
+#Ejercicio 5
+'''print('¿Cuantas horas trabajas?')
+hora=float(input())
+print('¿Cuanto te pagan por hora?')
+pago=float(input())
+print(hora*pago) '''
+
+#Ejercicio 6
+'''print('Ingrese un numero')
+n=int(input())
+print(((n*(n+1))/2))'''
+
+#Ejercicio 7
+'''print('¿Cuantas pesas? (Kg)')
+peso=float(input())
+print('¿Cuanto mides (m)?')
+estatura=float(input())
+n=(peso/estatura)**2
+m= round(n,2)
+print('Tu indice de masa corporal es', m)'''
+
+#Ejercicio 8 
+'''print('Ingrese un numero')
+n=int(input())
+print('Ingrese otro numero')
+m=int(input())
+mns= n/m
+mn= round(mns, 2)
+r= (n%m)
+c=round((n//m))
+print('el resultado de la division es', mn, 'el residuo es', r, 'y el cociente es', c)'''
+
+#Ejercicio 9
+'''print('Ingrese la cantidad que desea invertir')
+c=float(input())
+print('Ingrese el interes anual')
+i=float(input())
+print('Ingrese el numero de años')
+a=float(input())
+cap=(c*((i/100)**a))
+print('El capital obtenido por la inversion es', cap)'''
+
+#Ejercicio 10
