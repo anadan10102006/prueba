03f7cda5d0b25bc3e75e4b222bdfd11f77af27eb
anadan10102006 <141078706+anadan10102006@users.noreply.github.com>
2023-08-14 22:31:36 +0000

    ejercicio 14

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ if m>20: print(m, 'Es un gran numero')
 else: print('La suma de todos los enteros desde 1 hasta', n, 'es', m )'''
 
 #Ejercicio 12
-print('Ingrese un numero')
+'''print('Ingrese un numero')
 n=int(input())
 print('Ingrese otro numero')
 m=int(input())
@@ -86,4 +86,38 @@ c=round((n//m))
 print('el resultado de la division es', mn, 'el residuo es', r, 'y el cociente es', c)
 if c<1: print('El divisor es mayor al dividendo')
 else if c>1: print('El divisor es menor que el dividendo')
-else if c==1: print('El divisor y el dividendo son iguales') 
+else if c==1: print('El divisor y el dividendo son iguales')''' 
+
+
+#Ejercicio 13
+'''print('Ingrese la cantidad que desea invertir')
+c=float(input())
+print('Ingrese el interes anual')
+i=float(input())
+print('Ingrese el numero de años')
+a=float(input())
+cap=(c*((i/100)**a))
+print("el valor del capital es", cap)
+if cap<100000: print ("La inversion de", c, "tiene baja rentabilidad")
+if cap>100000 and cap<1000000: print("La inversion de", c, "tiene una rentabilidad moderada")
+if cap>1000000: print("La inversion de", c, "es una buena inversion")'''
+
+
+#Ejercicio 14
+print('Ingrese la cantidad de payasos vendidos en el ultimo pedido')
+pc=int(input())
+print('Ingrese la cantidad de muñecas vendidas en el ultimo pedido')
+mc=int(input())
+p= 112
+m= 75
+t= "si" 
+pcp= pc*p
+mcm= mc*m
+mmpp= mcm+pcp
+if mmpp>3000000: 
+  print("¿Desea enviarlo?")
+  r=input()
+  if r==t: 
+    print ("Contenedor enviado")
+  else: print("Contenedor no enviado")
+else: print('El peso total del pedido es', (mmpp/1000), 'kilos')
